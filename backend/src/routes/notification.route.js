9ec795c01cd5ce4d1f69c003cae8a57f39c56043
Arindamsharma12 <117537886+Arindamsharma12@users.noreply.github.com>
2025-10-19 01:02:24 +0530

    noti fix

--- backend/src/routes/notification.route.js
+++ backend/src/routes/notification.route.js
@@ -7,6 +7,6 @@ import {
 const router = express.Router();
 
 router.get("/", protectRoute, getNotifications);
-router.delete("/:notification", protectRoute, deleteNotification);
+router.delete("/:notificationId", protectRoute, deleteNotification);
 
 export default router;
